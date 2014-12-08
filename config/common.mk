4c28889604fd752436368b6219f3f6a89b4e0ffb
willl03 <wgangers@gmail.com>
2014-12-08 10:01:33 -0500

    Keep device stock DPI
    Change-Id: Ibacf67620a3af0887a8993da9cdbb39cd5cdded8

--- config/common.mk
+++ config/common.mk
@@ -176,6 +176,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.slim.version=$(SLIM_VERSION) \
     ro.modversion=$(SLIM_MOD_VERSION) \
     ro.slim.buildtype=$(SLIM_BUILD_TYPE)
-
-EXTENDED_POST_PROCESS_PROPS := vendor/slim/tools/slim_process_props.py
-
