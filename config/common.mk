8be91e3d92639c96d10b3f02b205baacd47cb9f4
willl03 <wgangers@gmail.com>
2014-12-13 23:46:02 -0500

    Add fstrim init.d script
    Change-Id: I14cd48df8efebd20ce076863842438b68e5f4753

--- config/common.mk
+++ config/common.mk
@@ -60,6 +60,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/slim/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
+    vendor/slim/prebuilt/common/etc/init.d/99fstrim:system/etc/init.d/99fstrim \
     vendor/slim/prebuilt/common/bin/sysinit:system/bin/sysinit
 
 # Embed SuperUser
